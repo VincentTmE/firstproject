670dcc351f4fc1f8709bde31b66e6e12133cad84
Vincent <vincent.ti@icloud.com>
2024-10-22 14:36:48 +0200

    Everything Woks - With merge

--- src/app/log-chart/log-chart.component.ts
+++ src/app/log-chart/log-chart.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component, AfterViewInit, input, AfterContentChecked } from '@angular/core';
+import { Component, AfterViewInit, input, AfterContentChecked, Input } from '@angular/core';
+import { EChartsOption } from 'echarts';
 import { NgxEchartsDirective, provideEcharts } from 'ngx-echarts';
+import { merge } from 'rxjs';
 
 
 
@@ -21,13 +23,15 @@ export class LogChartComponent  implements AfterContentChecked{
   isLogDisplayed: boolean = false
 
   chartOption: echarts.EChartsOption
+  updateOptions: any
+  
   
 
   // This method runs after the component's view is initialized
   ngAfterContentChecked(): void {
     // Make sure the DOM element is available after the view is initialize
-   
-  this.chartOption  = {
+   if (!this.isLogDisplayed)
+  this.chartOption  = { 
       xAxis: {
         type: 'category',
         data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
@@ -35,31 +39,25 @@ export class LogChartComponent  implements AfterContentChecked{
       yAxis: {
         type: 'value'
       },
-      series: [
+       series: [
         {
           data: [150, 230, 224, 218, 135, 147, 260],
           type: 'line'
         }
       ]
     };
-    if (this.isLogDisplayed) {
-        this.chartOption = {
-        xAxis: {
-          type: 'category',
-          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-        },
-        yAxis: {
-          type: 'log'
-        },
-        series: [
-          {
-            data: [150, 230, 224, 218, 135, 147, 260],
-            type: 'line'
-          } ]
+    if (this.isLogDisplayed)
+     this.updateOptions = {
+       ...this.chartOption,
+       
+       yAxis: {
+        type: 'log'
+       },
+      
       }
-    }
+      
     
-  }
+      }
   onSwitchFreqView() {
     this.isLogDisplayed = !this.isLogDisplayed 
     console.log(this.isLogDisplayed)
